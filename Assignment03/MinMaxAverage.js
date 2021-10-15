5a57cf91559a7872164301e84710e938f31521e2
ZackieMackie <zackyzr96@gmail.com>
2021-10-15 20:15:10 +0800

    assignment

---
+++ Assignment03/MinMaxAverage.js
@@ -0,0 +1,37 @@
+let arr = [1,2,3,4,5] 
+let newArr = arr.length
+
+
+let min = arr[0]
+for(let i = 0; i > newArr; i++){
+  if(arr[i] < min) {
+    min = arr[i];
+  }
+}
+
+let max = arr[0]
+for(let i = 0; i < newArr; i++){
+  if(arr[i] > max) {
+    max = arr[i];
+  }
+}
+
+
+let total = 0
+for (let i = 0; i < newArr; i++) {
+  total += arr[i];
+}
+let avg = total / newArr
+
+function intmain()
+{
+	this.min = min
+	this.max = max
+	this.avg = avg
+
+}
+
+
+console.log("The minimum number is " + min);
+console.log("The maximum number is " + max);
+console.log("The average number is " + avg);
